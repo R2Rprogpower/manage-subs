7760317edb2bfc785f229d5b7917499125475d51
ragimov <ragimov.ruslan.2000@gmail.com>
2026-03-14 21:18:19 +0000

    cd

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/health', static function (): \Illuminate\Http\JsonResponse {
     DB::connection()->getPdo(); // throws if DB is unreachable
+
     return response()->json(['status' => 'ok']);
 });
 

4f057a3fcb28cd97351a3ed8ad6bae977f7e1a73
ragimov <ragimov.ruslan.2000@gmail.com>
2026-09-03 11:36:58 +0000

    docker +

--- app/Models/TelegramChannel.php
+++ app/Models/TelegramChannel.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Database\Factories\TelegramChannelFactory;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
